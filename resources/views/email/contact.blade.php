ec0a75774714511877dde3c14f04219e66a22744
supal-s <supals1998@gmail.com>
2023-12-04 14:06:35 +0530

    page integration

--- resources/views/email/contact.blade.php
+++ resources/views/email/contact.blade.php
@@ -8,12 +8,12 @@
             <tbody>
                 <tr>
                     @php
-                    $data = $data->toArray();
+                    $extra_data = json_decode($data->extra_data);
 
                     $replace_keys = ['d_o_b'=>'date of birth'];
                     $remove_keys = ['id','created_at','updated_at','deleted_at'];
                     foreach($remove_keys as $remove_k)
-                    unset($data[$remove_k]);
+                    unset($extra_data[$remove_k]);
                     @endphp
                     <td>
                         <div class="thank-msg">
@@ -23,7 +23,26 @@
                         <p>You have received a submission.</p>
                         <table class="email-table">
                             <tbody>
-                                @foreach($data as $key=>$value)
+                                <tr>
+                                    <td>Name: </td>
+                                    <td> {{$data->name}}</td>
+                                </tr>
+
+                                @if($data->email)
+                                <tr>
+                                    <td>Email: </td>
+                                    <td> {{$data->email}}</td>
+                                </tr>
+                                @endif
+                                @if($data->phone_number )
+                                <tr>
+                                    <td>Phone: </td>
+                                    <td> {{$data->phone_number }}</td>
+                                </tr>
+                                @endif
+
+
+                                @foreach($extra_data as $key=>$value)
                                 @if (!empty($value))
                                 @php
                                 $key = isset($replace_keys[$key])?$replace_keys[$key]:$key;
@@ -34,6 +53,19 @@
                                 </tr>
                                 @endif
                                 @endforeach
+
+                                @if($data->message)
+                                <tr>
+                                    <td>Message: </td>
+                                    <td> {{$data->message}}</td>
+                                </tr>
+                                @endif
+                                @if($data->lead_type)
+                                <tr>
+                                    <td>lead_type: </td>
+                                    <td> {{$data->lead_type}}</td>
+                                </tr>
+                                @endif
                             </tbody>
                         </table>
                         <br>
